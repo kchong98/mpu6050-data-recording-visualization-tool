f5d822b410c02c2484eb01f2990f2eac8c8b8e66
XuLiangQi <76532517+XuLiangQi@users.noreply.github.com>
2021-09-29 15:14:08 -0300

    cleanning live_potting,py

--- live_plotting.py
+++ live_plotting.py
@@ -6,15 +6,6 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from mpl_toolkits.mplot3d import Axes3D
 
-# data=pd.read_csv("SensorDataFile2.csv", header=None)
-# data = pd.DataFrame.to_numpy(data)
-# data = data[1:, 3:6]
-
-# X = data[:, 0].astype(np.float)
-# Y = data[:, 1].astype(np.float)
-# Z = data[:, 2].astype(np.float)
-
-
 def getdisplacement(data):
     x_list = np.array([]).reshape(0, 1)
     v01 = 0
@@ -22,15 +13,11 @@ def getdisplacement(data):
     for i in range(2, len(data)):
         v0 = (1/2)*(data[i-1]+data[i-2])*0.2+v01
         v01 = v0
-        xf = (1/4)*(data[i]-data[i-1])*0.02+v0*0.2+x0
+        xf = (1/4)*(data[i]-data[i-1])*0.04+v0*0.2+x0
         x0 = xf
         x_list = np.vstack((x_list, x0))
     return x_list
-    
-# x_displacement = getdisplacement(X)
-# x_displacement = x_displacement.reshape(len(x_displacement), )
-# y_displacement = getdisplacement(Y).reshape(len(x_displacement), )
-# z_displacement = getdisplacement(Z).reshape(len(x_displacement), )
+
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
@@ -40,7 +27,7 @@ plt.style.use('fivethirtyeight')
 index = count()
 
 def animate(i):
-    data = pd.read_csv('SensorDataFile2.csv')
+    data = pd.read_csv('<Name of the csv file>.csv') # Name of the CSV file needs to be configured
     data = pd.DataFrame.to_numpy(data)
     data = data[1:, 3:6]
 
@@ -51,17 +38,13 @@ def animate(i):
     y1 = x_displacement.reshape(len(x_displacement), )
     y2 = getdisplacement(Y).reshape(len(x_displacement), )
     y3 = getdisplacement(Z).reshape(len(x_displacement), )
-    # x = data['x_value']
-    # y1 = data['total_1']
-    # y2 = data['total_2']
-    # y3 = data['total_3']
 
     plt.cla()
     plt.plot(y1, y2, y3, label='Random 3D data')
     plt.legend(loc='upper left')
     plt.tight_layout()
 
-ani = FuncAnimation(plt.gcf(), animate, interval=1000)
+ani = FuncAnimation(plt.gcf(), animate, interval=50)
 
 plt.tight_layout()
 plt.show()
